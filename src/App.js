ef2ed12395110d5a6f3c82a50bf176b5fde16585
aantenore <ant_ant95@hotmail.it>
2021-06-19 15:40:11 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+
+  state = {}
+
+  inquire = () => {
+    let name = this.state.name
+    let keywords = this.state.keywords
+
+    if (name?.trim() != "" && (keywords?.trim().indexOf(',') > 0 || (keywords?.trim() != "" && keywords?.trim().indexOf(',') < 0))) {
+      let splittedKeywords = keywords.split(',').filter(k => k.trim() != "").map(k => encodeURIComponent(k));
+      let url = process.env.REACT_APP_SERVER_URL+"/inquire?n=" + name + "&k=" + JSON.stringify(splittedKeywords)
+      axios.get(url).then(res => console.log(res.data))
+    }
+  }
+
+  handleTextChange = (e) => {
+    let newState = {}
+    newState[e.target.id]=e.target.value
+    this.setState(newState)
+  }
+
+  render = () => (
+    <>
+      <div>
+        <label for="keywords">Keywords divided by commas (,): </label>
+        <input type="text" id="keywords" name="keywords" onChange={this.handleTextChange}/>
+      </div>
+      <div>
+        <label for="keywords">Your Name: </label>
+        <input type="text" id="name" name="name" onChange={this.handleTextChange}/>
+      </div>
+      <input type="submit" value="Inquire" onClick={this.inquire}/>
+    </>
   );
 }
 
