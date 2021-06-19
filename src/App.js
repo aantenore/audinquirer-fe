931c480c9e2d44a34b922c6c618c7a8048eabd00
aantenore <ant_ant95@hotmail.it>
2021-06-19 15:58:10 +0200

    deploy test

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import './App.css';
 import { Component } from 'react';
 import axios from 'axios';
+import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 
-  state = {}
+  state = {
+    spinnerActive: false
+  }
 
   inquire = () => {
     let name = this.state.name
     let keywords = this.state.keywords
 
-    if (name?.trim() != "" && (keywords?.trim().indexOf(',') > 0 || (keywords?.trim() != "" && keywords?.trim().indexOf(',') < 0))) {
-      let splittedKeywords = keywords.split(',').filter(k => k.trim() != "").map(k => encodeURIComponent(k));
-      let url = process.env.REACT_APP_SERVER_URL+"/inquire?n=" + name + "&k=" + JSON.stringify(splittedKeywords)
-      axios.get(url).then(res => console.log(res.data))
+    if (name?.trim() !== "" && (keywords?.trim().indexOf(',') > 0 || (keywords?.trim() !== "" && keywords?.trim().indexOf(',') < 0))) {
+      let splittedKeywords = keywords.split(',').filter(k => k.trim() !== "").map(k => encodeURIComponent(k));
+      let url = process.env.REACT_APP_SERVER_URL + "/inquire?n=" + name + "&k=" + JSON.stringify(splittedKeywords)
+      this.setState({ spinnerActive: true }, () => axios.get(url)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err=>{
+        console.error(err)
+      })
+      .finally(()=>{
+        this.setState({ spinnerActive: false })
+      })
+      );
     }
   }
 
   handleTextChange = (e) => {
     let newState = {}
-    newState[e.target.id]=e.target.value
+    newState[e.target.id] = e.target.value
     this.setState(newState)
   }
 
   render = () => (
-    <>
+    this.state.spinnerActive ?
       <div>
-        <label for="keywords">Keywords divided by commas (,): </label>
-        <input type="text" id="keywords" name="keywords" onChange={this.handleTextChange}/>
+        <Spinner animation="grow" />
       </div>
-      <div>
-        <label for="keywords">Your Name: </label>
-        <input type="text" id="name" name="name" onChange={this.handleTextChange}/>
-      </div>
-      <input type="submit" value="Inquire" onClick={this.inquire}/>
-    </>
+      :
+      <>
+        <div>
+          <label for="keywords">Keywords divided by commas (,): </label>
+          <input type="text" id="keywords" name="keywords" onChange={this.handleTextChange} />
+        </div>
+        <div>
+          <label for="keywords">Your Name: </label>
+          <input type="text" id="name" name="name" onChange={this.handleTextChange} />
+        </div>
+        <input type="submit" value="Inquire" onClick={this.inquire} />
+      </>
   );
 }
 
