68454647e71f6f26e719439f326dc66e47dada20
Antonio Antenore <ant_ant95@hotmail.it>
2021-06-28 00:50:29 +0200

    log removed

--- src/App.js
+++ src/App.js
@@ -66,7 +66,6 @@ class App extends Component {
   handleTextChange = (e) => {
     let newState = {}
     newState[e.target.id] = e.target.value
-    console.log(newState)
     this.setState(newState)
   }
 
