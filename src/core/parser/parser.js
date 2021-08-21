2413e6d6fdd96f64506e19e8d4c47361f012bf96
Antonio Antenore <ant_ant95@hotmail.it>
2021-08-21 17:58:53 +0200

    Revert "fix"
    This reverts commit 208136a52cc44e7d167feaf85ac0dc2d8a0854f1.

--- src/core/parser/parser.js
+++ src/core/parser/parser.js
@@ -7,7 +7,7 @@ const header = {
 }
 
 var parser = {
-    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url},{headers:header}),
+    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url, headeer},{headers:header}),
     getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url},{headers:header}),
     getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url},{headers:header}),
     getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url})
