700f644a0403ab2ddbcfecc6ef17db60e28b49e4
Antonio Antenore <ant_ant95@hotmail.it>
2021-08-21 17:52:56 +0200

    cors header

--- src/core/parser/parser.js
+++ src/core/parser/parser.js
@@ -1,10 +1,15 @@
 import axios from "axios"
 
+const header = {
+    'Access-Control-Allow-Methods':'POST',
+    'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+    'Access-Control-Allow-Origin':'*'
+}
 
 var parser = {
-    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url}),
-    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url}),
-    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url}),
+    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url, headeer},{headers:header}),
+    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url},{headers:header}),
+    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url},{headers:header}),
     getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url})
 }
 
