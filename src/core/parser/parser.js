131ae406ed11c7681e857c1912ef3e0a29c2a65b
Antonio Antenore <ant_ant95@hotmail.it>
2021-08-24 01:03:05 +0200

    Revert "retry + parallelism"
    This reverts commit d8e5066cc62df4fac0c5035d8c80a2cf1f7aee2a.

--- src/core/parser/parser.js
+++ src/core/parser/parser.js
@@ -2,11 +2,10 @@ import axios from "axios"
 
 
 var parser = {
-    //each call has a retry if it fails => throw error
-    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url}).catch(()=>axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url})).catch(e =>{console.log(e); throw e}),
-    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url}).catch(()=>axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url})).catch(e =>{console.log(e); throw e}),
-    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url}).catch(()=>axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url})).catch(e =>{console.log(e); throw e}),
-    getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url}).catch(e =>{console.log(e); throw e}) //not used for now
+    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url}).catch(e =>{console.log(e); throw e})
 }
 
 export default parser
