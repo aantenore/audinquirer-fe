2ac50d63c4e9242434d62a7dbd2020de449928e1
Antonio Antenore <ant_ant95@hotmail.it>
2021-08-23 10:57:26 +0200

    error log

--- src/core/parser/parser.js
+++ src/core/parser/parser.js
@@ -2,10 +2,10 @@ import axios from "axios"
 
 
 var parser = {
-    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url}),
-    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url}),
-    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url}),
-    getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url})
+    getBooks: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBooks',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookDetails: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookDetails',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookUrl: (url, name) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookUrl',{url:url}).catch(e =>{console.log(e); throw e}),
+    getBookHaveBulletPointInDescription: (url) => axios.post(process.env.REACT_APP_SERVER_URL+'/getBookHaveBulletPointInDescription',{url:url}).catch(e =>{console.log(e); throw e})
 }
 
 export default parser
